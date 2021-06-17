b1b90187b6c510e8d05ab3534f5d83d87a543e38
JoeKAboagye <Josephkwabenaaboagye@gmail.com>
2021-06-17 09:45:18 -0700

    Prints the text in the file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ *Entry point - "main"
+ *
+ *Always "return(0)", which means success!
+ */
+
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return(0);
+}
