5bfb26f0da2c7c40e62611b48259080fb193ded5
JoeKAboagye <Josephkwabenaaboagye@gmail.com>
2021-06-17 09:55:16 -0700

    Prints the text in the code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /*
- *Entry point - "main"
+ *main - entry point of code
  *
- *Always "return(0)", which means success!
+ *Always "return (0)", which means success!
  */
 
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
