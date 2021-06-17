740925597a641927388600835f77bda55cca86f7
JoeKAboagye <Josephkwabenaaboagye@gmail.com>
2021-06-17 12:10:50 -0700

    Prints a text in the code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-/*
- *main - entry point of code
+/**
+ * main - Entry point of code
  *
- *Always "return (0)", which means success!
+ * Return: Always 0 (success)
  */
 
 int main(void)
