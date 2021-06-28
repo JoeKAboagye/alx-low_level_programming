e4d98bd18d3fdb1105d9b9bb35516c39fe5ef829
JoeKAboagye <Josephkwabenaaboagye@gmail.com>
2021-06-28 13:15:49 -0700

    Converts string to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,36 +8,29 @@
 
 int _atoi(char *s)
 {
-	int c = 0;
-	int number;
+int i;
+int z, p;
 
-	while (*s)
-	{
-		if (*s == '-')
-		{
-			c++;
-			s++;
-		}
-		else if (*s >= '0' && *s <= '9')
-		{
-			number = *s - '0';
-			s++;
-			while (*s >= '0' && *s <= '9')
-			{
-				number = (number * 10) + (*s - '0');
-				s++;
-			}
-			if (c % 2 == 1)
-				return (number * (-1));
-			else
-				return (number);
+z = 0;
+p = -1;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] == '-')
+p *= -1;
 
-		}
-		else
-			s++;
+if (s[i] > 47 && s[i] < 58)
+{
+if (z < 0)
+z = (z * 10) - (s[i] - '0');
+else
+z = (s[i] - '0') * -1;
 
+if (s[i + 1] < 48 || s[i + 1] > 57)
+break;
+}
+}
+if (p < 0)
+z *= -1;
 
-	}
-	return (0);
-
+return (z);
 }
