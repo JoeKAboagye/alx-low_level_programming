52141362eb463c81cf5d0f091f8fc59c14e1876d
JoeKAboagye <Josephkwabenaaboagye@gmail.com>
2021-11-24 18:23:15 -0800

    python module

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Island Perimeter
+A module for working with grid structures.
+"""
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island"""
+    patches = 0
+    borders = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                patches += 1
+                if row != len(grid) - 1:
+                    if grid[row + 1][col] == 1:
+                        borders += 1
+                if col != len(grid[row]) - 1:
+                    if grid[row][col + 1] == 1:
+                        borders += 1
+    perimeter = (patches * 4) - (borders * 2)
+    return perimeter
