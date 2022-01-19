c39892031c73321df3c9f1e507bd1590421072e7
Joseph Aboagye <josephkwabenaaboagye@gmail.com>
2022-01-19 19:28:31 +0000

    Uses Linear search algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,19 +9,22 @@
  * Return: first index where value is located,
  *         or -1 if value is not present or array is NULL.
  **/
+
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (!array)
-		return (-1);
-
-	for (i = 0; i < size; i++)
+	for (i = 0; (i < size) && (array); i++)
 	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (array[i] == value)
+		if (*(array + i) == value)
+		{
+			printf("Value checked array[%d] = [%d]\n", (int)i, *(array + i));
 			return (i);
+		}
+		else
+		{
+			printf("Value checked array[%d] = [%d]\n", (int)i, *(array + i));
+		}
 	}
-
 	return (-1);
 }
