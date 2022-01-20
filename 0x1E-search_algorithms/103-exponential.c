432b2141d5de4141430a3848fb5a905516b98eac
Joseph Aboagye <josephkwabenaaboagye@gmail.com>
2022-01-20 01:46:44 +0000

    Uses Exponential search algorithm

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,11 @@
+#include "search_algos.h"
+
+/**
+ * exponential_search - searches for a value in a sorted array of integers,
+ *                      using the Exponential search algorithm.
+ * @array: pointer to the first element of the array to search in.
+ * @size: number of elements in array.
+ * @value: index where value is located.
+ * Return: first index where value is located,
+ *         or -1 if value is not present or array is NULL.
+ **/
