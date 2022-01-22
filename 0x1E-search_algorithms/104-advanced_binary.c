4d5605f303db4d344e9f79ed7a2e3c59cef316e6
Joseph Aboagye <josephkwabenaaboagye@gmail.com>
2022-01-22 16:55:51 +0000

    Update 104-advanced_binary.c

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * advanced_binary_recursive - Searches recursively for a value in a sorted
+ * advance_binary_recursive - Searches recursively for a value in a sorted
  *                             array of integers using binary search.
  * @array: A pointer to the first element of the [sub]array to search.
  * @left: The starting index of the [sub]array to search.
@@ -50,5 +50,5 @@ int advanced_binary(int *array, size_t size, int value)
 	if (array == NULL || size == 0)
 		return (-1);
 
-	return (advanced_binary_recursive(array, 0, size - 1, value));
+	return (advance_binary_recursive(array, 0, size - 1, value));
 }
